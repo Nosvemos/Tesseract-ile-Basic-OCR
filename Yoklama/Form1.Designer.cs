4e37562d24e71d01085c1ea4fb080d79c276802b
Nosvemos <53067521+Nosvemos@users.noreply.github.com>
2020-12-12 00:14:01 +0300

    Final

---
+++ Yoklama/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Yoklama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SelectFile1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 397);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // SelectFile1
+            // 
+            this.SelectFile1.Location = new System.Drawing.Point(12, 12);
+            this.SelectFile1.Name = "SelectFile1";
+            this.SelectFile1.Size = new System.Drawing.Size(242, 23);
+            this.SelectFile1.TabIndex = 3;
+            this.SelectFile1.Text = "Click";
+            this.SelectFile1.UseVisualStyleBackColor = true;
+            this.SelectFile1.Click += new System.EventHandler(this.selectFiles1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(266, 447);
+            this.Controls.Add(this.SelectFile1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tesseract";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SelectFile1;
+    }
+}
+
---
+++ Yoklama/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace Yoklama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        Bitmap image = new Bitmap("Resources/empty.png");
+        private void selectFiles1(object sender, EventArgs e)
+        {
+            var OpenFileDialog = new OpenFileDialog();
+            if(OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var image = new Bitmap(OpenFileDialog.FileName);
+                var ocr = new TesseractEngine("./tessdata","eng");
+                var sonuc = ocr.Process(image);
+                textBox1.Text = sonuc.GetText();
+            }
+        }
+    }
+}
